6b2254be915cf35cba252f8fe2a85e6148554e33
Ernesto776 <galianaernesto@gmail.com>
2025-11-23 23:52:51 -0800

    Expanded on by adding check_dti_ratio option to the main menu. Created the beginning structure for add_income, add_debt, and check_dti_ratio functions.

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main_menu():
     print("Welcome to the Main Menu")
     print("1. Enter Income")
     print("2. Enter Debt")
-    print("3. Exit")
+    print("3. Check Debt-to-Income Ratio")
+    print("4. Exit")
 
     choice = input("Please select an option: ")
 
@@ -11,7 +12,24 @@ def main_menu():
     elif choice == '2':
         enter_debt()
     elif choice == '3':
+        check_dti_ratio()
+    elif choice == '4':
         print("Exiting the program. Goodbye!")
     else:
         print("Invalid choice. Please try again.")
         main_menu()
+
+def enter_income():
+    hold_percent = 0
+    tax_percent = 0
+    net_income = 0
+    main_menu()
+
+def enter_debt():
+    debt_collection = []
+    user_command = input(f'Enter debt amount or “finish” to finish or “undo” to remove last input: ').lower
+    debt = input("Please enter your debt: ")
+    main_menu()
+
+def check_dti_ratio():
+    main_menu()
